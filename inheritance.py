2274bb42eab4f8ca4ad6b39a94134e4033ef52d2
raj@9883122_MLnDS <rksingh9883122@gmail.com>
2025-07-27 00:33:11 +0530

    object & class_Inheritancet

---
+++ inheritance.py
@@ -0,0 +1,22 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        
+    def speak(self):
+        print(f"{self.name} makes sound")
+        
+        
+
+class Dog(Animal):
+    def speak(self):
+        print(f"{self.name} barks")
+    
+animal = Animal("Generic Animal")
+animal.speak()
+
+        
+dog = Dog('Jacky')
+dog.speak()
+           
+           
+
